2782da6f29884222d6ad9df902aa896ec7fac9b2
tinle15 <tinle1596@gmail.com>
2022-03-03 07:49:27 -0800

    add ability to scroll to current time on refresh

--- src/store/useItinerary.js
+++ src/store/useItinerary.js
@@ -17,8 +17,20 @@ export const useItineraryStore = defineStore("Itinerary", {
                 const snap = await getDocs(q);
                 snap.forEach((doc) => {
                     results.push({id: doc.id, ...doc.data()});
-                })                
+                })
+                //sort by id
                 this.event = results.sort((a,b) => (parseFloat(a.id) - parseFloat(b.id)))
+                
+                let date = null
+                //create timestamp for href
+                this.event = this.event.map(x=> {
+                    if(x.hasOwnProperty('date')){
+                        date = x.date
+                    }
+                    x.timeStamp = date + ', ' + x.startTime
+                    return x;
+                })                
+
             } catch (e) {
                 alert(e.message)
             }
